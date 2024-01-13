c08b2934039e838ed19abcf3f5a36c37129ebfa5
_ñ <knaka0209@gmail.com>
2024-01-14 08:21:49 +0900

    up, 0114a4

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,26 @@ const app = new Hono();
 app.use("/*", cors());
 //
 import App from './pages/App';
+/*
+API
+*/
+app.get('/api/test1', async (c) => { 
+  const resulte = {ret:"OK", msg:"" };
+  return c.json(resulte);
+});
+app.get('/api/test2', async (c) => {
+  const result = await c.env.DB.prepare(`SELECT * FROM todos ORDER BY id DESC`).all();
+console.log(result.results);
+  return c.html(
+    renderToString(
+    <div>
+      <h1>Hello!</h1>
+      {JSON.stringify(result.results)}
+    </div>
+    )
+  )
+});
+
 //
 app.get('/*', async (c) => { 
   return c.html(renderToString(App([])));
